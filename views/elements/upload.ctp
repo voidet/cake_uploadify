bf3688f21c46adc680aa7cb8e38aac296562f62b
Richard Sbresny <voidet@nightgen.com>
2010-09-22 13:02:47 +1000

    Sortable and fieldnames

--- views/elements/upload.ctp
+++ views/elements/upload.ctp
@@ -2,4 +2,5 @@
 <div id="<?php echo $settings['uuid']; ?>" class="uploadBin"></div>
 <script type="text/javascript">
 	$(document).trigger('attachUploadify', <?php echo $this->Javascript->object($settings); ?>);
+	$('.uploadBin').sortable({ opacity: 0.6, tolerance: 'pointer', items: '.uploadItem' });
 </script>
--- webroot/js/cake_uploadify.js
+++ webroot/js/cake_uploadify.js
@@ -1,4 +1,5 @@
 $(function() {
+
 	$(document).bind('attachUploadify', function(event, settings) {
 		$(".uploadify."+settings.uuid).uploadify({
 			'uploader': '/cake_uploadify/flash/uploadify.swf',
@@ -16,7 +17,8 @@ $(function() {
 		function updateFileInfo(event, queueId, fileObj, response, data) {
 			var upload = $.parseJSON(response);
 			if (upload.Image.length != 0) {
-				$('#'+upload.metadata.uuid+'.uploadBin').append('<img src="/generated/images/'+upload.Image.slug+'_w100_h100.jpg" /><input type="hidden" name="'+upload.metadata.inputName+'[Image][][image_id]" value="'+upload.Image.id+'" /><input type="hidden" name="'+upload.metadata.inputName+'[Image][][image_position]" value="" />');
+				$('#'+upload.metadata.uuid+'.uploadBin').append('<div class="uploadItem"><img src="/generated/images/'+upload.Image.slug+'_w100_h100.jpg" /><input type="hidden" name="'+upload.metadata.inputName+'[Image]['+upload.Image.id+'][image_id]" value="'+upload.Image.id+'" /><input type="hidden" name="'+upload.metadata.inputName+'[Image]['+upload.Image.id+']['+upload.metadata.position_field+']" value="" class="uploadPosition" /></div>');
+				$('.uploadBin').sortable('refresh');
 			}
 		}
 	});
@@ -24,15 +26,13 @@ $(function() {
 	$('#PostAdminForm').submit(function() {
 		var order = 0;
 		$('.uploadBin').each(function() {
-			console.debug($(this));
 			$(this).filter(function() {
-					console.debug($(this).find('input[type=hidden]').attr('name').match('position'));
+					$(this).find('.uploadItem input[type=hidden].uploadPosition');
 				}).each(function() {
 					order++;
-					$(this).attr('value', order)
+					$(this).attr('value', order);
 			});
 		});
-		return false;
 	});
 
 });
