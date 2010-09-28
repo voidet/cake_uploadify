b9c63b6ef78666f99ddfa88f1cf645d524e26109
Richard Sbresny <voidet@nightgen.com>
2010-09-28 16:07:10 +1000

    Added in trnsfr

--- views/elements/upload.ctp
+++ views/elements/upload.ctp
@@ -1,6 +1,7 @@
 <?php echo $form->input('CakeUploadify-'.$settings['uuid'], array('type' => 'file', 'label' => $settings['label'], 'class' => 'uploadify '.$settings['uuid'])); ?>
-<div id="<?php echo $settings['uuid']; ?>-fileQueue"></div>
+<div id="<?php echo $settings['uuid']; ?>-trnsfr"></div>
 <div id="<?php echo $settings['uuid']; ?>" class="uploadBin"></div>
+<div id="<?php echo $settings['uuid']; ?>-speed" class="uploadSpeed"></div>
 <script type="text/javascript">
 	$(document).trigger('attachUploadify', <?php echo $this->Javascript->object($settings); ?>);
 	$('.uploadBin').sortable({ opacity: 0.6, tolerance: 'pointer', items: '.uploadItem' });
--- webroot/js/cake_uploadify.js
+++ webroot/js/cake_uploadify.js
@@ -1,29 +1,60 @@
 $(function() {
 
 	$(document).bind('attachUploadify', function(event, settings) {
-		console.debug(settings);
+
+		var files = [];
+		var trnsfr = Object;
+		var settings = settings;
+		var total = 0;
+
 		$("#CakeUploadify-"+settings.uuid).uploadify({
 			'uploader': '/cake_uploadify/flash/uploadify.swf',
 			'script': settings.script,
 			'method': 'POST',
 			'scriptData': settings,
-			'cancelImg': '/cake_uploadify/img/cancel.png',
+			'cancelImg': '/cake_uploadify/img/close.png',
 			'folder': settings.folder,
-			'queueID': settings.uuid+'-fileQueue',
+			'queueID': 'null',
 			'auto': true,
+			'width': 112,
 			'multi': true,
-			'onComplete': updateFileInfo,
+			'onComplete': onItemComplete,
+			'onProgress': updateTrnsfrProgress,
+			'onSelectOnce': addFilesToTrnsfer,
+			'onSelect': onSelectItems,
+			'onAllComplete': cleanUp,
 		});
 
-		function updateFileInfo(event, queueId, fileObj, response, data) {
+		function addFilesToTrnsfer(event, data) {
+			trnsfr = $('#'+settings.uuid+'-trnsfr').trnsfr(files);
+		}
+
+		function cleanUp() {
+			//$('#'+settings.uuid+'-trnsfr, #'+settings.uuid+'-speed').delay(1000).fadeOut('slow');
+		}
+
+		function onSelectItems(event, queueId, fileObj) {
+			files.push(fileObj.size);
+		}
+
+		function updateTrnsfrProgress(event, queueId, fileObj, data) {
+			$('#'+settings.uuid+'-speed').html(data.speed+'KB/s');
+			trnsfr.setData(data.allBytesLoaded);
+			console.debug(data.allBytesLoaded );
+			console.debug(files);
+		}
+
+		function onItemComplete(event, queueId, fileObj, response, data) {
+//			var file = files.shift();
+
 			var upload = $.parseJSON(response);
 			if (upload.Image.length != 0) {
 				var width = Number(upload.metadata.width);
 				var height = Number(upload.metadata.height);
 
 				var item = $('#'+upload.metadata.uuid+'.uploadBin').append('<div class="uploadItem" style="width: '+ (width + 10) +'px; height: '+ (height + 10) +'px;"><img src="/cake_uploadify/img/close.png" height="25" width="25" alt="Remove Item?" border="0" class="cakeUploadify-removeItem" /><img src="/generated/images/'+upload.Image.slug+'_w'+ width +'_h100.jpg" class="uploadItemImage" /><input type="hidden" name="'+upload.metadata.inputName+'[Image]['+upload.Image.id+'][image_id]" value="'+upload.Image.id+'" /><input type="hidden" name="'+upload.metadata.inputName+'[Image]['+upload.Image.id+']['+upload.metadata.position_field+']" value="" class="uploadPosition" /></div>');
+
 				$('.uploadItem').hover(function() {
-					console.debug($(this));
 					$(this).find('.cakeUploadify-removeItem').fadeIn('fast');
 				}, function() {
 					$(this).find('.cakeUploadify-removeItem').fadeOut('fast');
@@ -34,7 +65,6 @@ $(function() {
 						$(this).remove();
 					});
 				});
-
 				$('.uploadBin').sortable('refresh');
 			}
 		}
